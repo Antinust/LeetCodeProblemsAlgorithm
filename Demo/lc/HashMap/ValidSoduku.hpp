cb5dfc910f7c40d33569ced10cca5c717690ec23
zhuochuncai <zhuochuncai@baidu.com>
2022-04-06 14:40:12 +0800

    [lc][hash_map]validSoduku

--- Demo/lc/HashMap/ValidSoduku.hpp
+++ Demo/lc/HashMap/ValidSoduku.hpp
@@ -10,11 +10,51 @@
 #define ValidSoduku_h
 #include "PublicCommon.h"
 
+namespace ValidSoduku {
+/*
+ 1. use hash map to tell if duplicate digit exist in each row or column or 3x3 sub-box
+ mind the border/corner case
+ */
+
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        return false;
+        int m = board.size();
+        int n = board[0].size();
+        for (int i = 0; i < m; i++) {
+            unordered_map<int, int>map;
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] != '.' && ++map[board[i][j]] > 1) {
+                    return false;
+                }
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            unordered_map<int, int>map;
+            for (int i = 0; i < m; i++) {
+                if (board[i][j] != '.' && ++map[board[i][j]] > 1) {
+                    return false;
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; i += 3) {
+            for (int j = 0; j < n; j+=3) {
+                unordered_map<int, int>map;
+                for (int k = i; k < i + 3; k++) {
+                    for (int l = j; l < j + 3; l++) {
+                        if (board[k][l] != '.' && ++map[board[k][l]] > 1) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 };
+
+}
+
 
 #endif /* ValidSoduku_h */
